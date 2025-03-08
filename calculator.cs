0f49a93e6d11dacab3e223f020e79f701a94c61a
MSohaibAhmad <syedsohaibahmad4u@gmail.com>
2025-03-08 12:46:56 +0500

    Update addition calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -5,6 +5,15 @@ class Program
     static void Main()
     {
         Console.WriteLine("Muhammad Soahib Ahmad");
-        Console.Writeline("24-ST-040")
+        Console.Writeline("24-ST-040");
+        
+         Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Sum: {a + b}");
+            
     }
 }
